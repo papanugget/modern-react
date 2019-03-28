b48d470c6852a342a5e95040746c9e5490be0b07
papanugget <evilgenius04@gmail.com>
2019-03-28 14:51:55 -0400

    added first project

---
+++ first-app/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+// import logo from './logo.svg';
+import './App.css';
+
+function transform(offset) {
+  const cos = Math.cos(offset);
+  const sin = Math.sin(offset);
+  return { transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)` };
+}
+
+class App extends Component {
+  state = {
+    styleOne: {},
+    styleTwo: {}
+  };
+  onMouseMove = (e) => {
+    this.setState({
+      styleOne: transform(-e.clientX / e.clientY),
+      styleTwo: transform(e.clientX / e.clientY),
+    })
+  }
+  render() {
+    return <div onMouseMove={this.onMouseMove}>
+      <div className="panel" style={this.state.styleOne}/>
+      <div className="panel" style={this.state.styleTwo}/>
+
+    </div>
+  }
+}
+
+ReactDom.render(
+  <App/>,
+  document.querySelector('#root')
+);
+export default App;
