050005413ec21e8cd33b00d3752458450c51ae60
papanugget <evilgenius04@gmail.com>
2019-04-16 15:57:28 -0400

    added youtube api credentials and axios

---
+++ projects/youtube-search/src/api/youtube.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const KEY ='AIzaSyAao_MWF0Uh6yURapF3oyYQkrvpLZeI-oM';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        parts: 'snippet',
+        maxResults: 5,
+        key: KEY 
+    }
+})
