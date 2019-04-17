2425953c9cc2d2647dc70e3f41f61e2c24e99181
papanugget <evilgenius04@gmail.com>
2019-04-17 15:29:33 -0400

    added youtube state update code

--- projects/youtube-search/src/components/App.js
+++ projects/youtube-search/src/components/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
-    onTermSubmit = (term) => {
-        console.log(term);
-        youtube.get('/search', {
+    // add state to contain returned videos data
+    state = {
+        videos: []
+    };
+
+    // refactor to async func
+    onTermSubmit = async (term) => {
+        // console.log(term);
+        const res = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
+        // console.log(res);
+        this.setState({ videos: res.data.items})
+        // console.log(this.state);
     };
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+                I found {this.state.videos.length} videos.
+                <VideoList videos={this.state.videos}/>
             </div>
         )
     }
--- projects/youtube-search/src/components/VideoList.js
+++ projects/youtube-search/src/components/VideoList.js
@@ -1,3 +1,14 @@
 import React from 'react';
 
+const VideoList = (props) => {
+    console.log(props.videos);
+    const videos = props.videos;
+    videos.map( video => {
+        
+    })
+    return (
+        <div>VideoList here</div>
+    )
+}
+
 export default VideoList;
