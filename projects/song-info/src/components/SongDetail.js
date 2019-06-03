4351108ce19e3d3d2b95d542123db64f889257c2
papanugget <evilgenius04@gmail.com>
2019-06-03 11:28:55 -0400

    conditional render for details added

--- projects/song-info/src/components/SongDetail.js
+++ projects/song-info/src/components/SongDetail.js
@@ -2,11 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 // import { statement } from '@babel/template';
 
-const SongDetail = (props) => {
-    console.log(props);
+const SongDetail = ({song}) => {
+    console.log(song);
     return (
-        <div>Song Detail</div>
+        !song ? <h3>Please select a song</h3> : <div>
+            <h3>Details for: </h3>
+            <p>
+                Title: {song.title}
+                <br/>
+                Aritst: {song.artist}
+                <br/>
+                Genre: {song.genre}
+            </p>
+        </div>
     )
+    // if(!song) {
+    //     return <div>Please select a song</div>
+    // } else {
+    //     return (
+    //         <div className="container">
+    //             <div>{song.artist}</div>
+    //             <div>{song.title}</div>
+    //             <div>{song.genre}</div>
+    //         </div>
+
+    //     )
+    // }
 };
 
 const mapStateToProps = (state) => {
