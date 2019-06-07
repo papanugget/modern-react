77ef251b6f0992afa16ce37728bcb3e99f5b4306
papanugget <evilgenius04@gmail.com>
2019-06-07 11:44:23 -0400

    refactored action/index with redux-thunk

--- projects/biz-blog/src/actions/index.js
+++ projects/biz-blog/src/actions/index.js
@@ -1,11 +1,10 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () => {
-    // bad approach
-    const resp = await jsonPlaceholder.get('/posts');
+export const fetchPosts = () => async dispatch => {
+        const response = await jsonPlaceholder.get('/posts');
 
-    return {
-        type: 'FETCH_POSTS',
-        payload: resp
-    };
-};
+        dispatch({
+            type: 'FETCH_POSTS',
+            payload: response
+        })
+};        
