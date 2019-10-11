38ceb731674c50d0cbeab088be179ba449b1ebdc
papanugget <evilgenius04@gmail.com>
2019-10-11 15:59:15 -0400

    added redux-forms

--- projects/streams/client/src/components/streams/StreamCreate.js
+++ projects/streams/client/src/components/streams/StreamCreate.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return(
-        <div>
-            StreamCreate here
-        </div>
-    );
-}
+class StreamCreate extends React.Component {
+    renderInput({ input }) {
+        // console.log(formProps
+        return <input {...input} />;
+    };
+    render() {
+        return (
+            <form>
+                <Field name="title" component={this.renderInput}/>
+                <Field name="description" component={this.renderInput}/>
+            </form>
+         );
+    };
+};
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'STREAM_CREATE'
+})(StreamCreate);
--- projects/streams/client/src/reducers/index.js
+++ projects/streams/client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import { reducer as formReducer }  from 'redux-form';
 
 export default combineReducers({
     // dummyReducer: () => 1234556
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
